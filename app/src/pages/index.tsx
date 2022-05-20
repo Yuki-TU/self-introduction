0e8daf25a4fd6165cd70b3a648fd65423127d347
Yuki-TU <k.yuki.90219@icloud.com>
2022-05-20 22:22:17 +0900

    add:#9 タブレット表示がうまく表示できていなかったので、固定幅にした

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -9,7 +9,7 @@ import { Footer, Header } from "../components/uniqueParts";
  */
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col mx-auto w-full min-h-screen md:w-[1230px]">
       <Head>
         <title>y&apos;s room</title>
         <meta name="description" content="自己紹介のトップページです。" />
